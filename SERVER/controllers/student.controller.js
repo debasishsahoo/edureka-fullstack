fbce10dd5f644da07b4c55d47478a303e1595060
debasish <debasish.sahoo96@gmail.com>
2024-09-10 21:53:03 +0530

    Student CRUD Coplete

--- SERVER/controllers/student.controller.js
+++ SERVER/controllers/student.controller.js
@@ -1,11 +1,19 @@
+const mongoose = require("mongoose");
 const studentServices = require("../services/student.services");
 
 const addStudent = async (req, res) => {
   const { regId, name, course, section } = req.body;
   try {
-    await studentServices.insertData({ regId, name, course, section });
-    return res.status(200).send({ message: "success", error: null });
-  } catch (err) {
+    const newStudent = await studentServices.insertData({
+      regId,
+      name,
+      course,
+      section,
+    });
+    return res
+      .status(200)
+      .send({ message: "success", error: null, data: newStudent });
+  } catch (error) {
     if (error.keyValue.title === title) {
       return res
         .status(400)
@@ -16,18 +24,111 @@ const addStudent = async (req, res) => {
       .send({ message: "failure", error: "Internal server error" });
   }
 };
+const getStudent = async (req, res) => {
+  try {
+    const allStudent = await studentServices.getAllData();
+    if (!allStudent) {
+      return res
+        .status(404)
+        .send({ message: "No Data Found", error: null, data: [] });
+    }
+    const studentCount = allStudent.length;
 
-const getStudent=async(req,res)=>{
-  return res.status(200).send({ message: 'getStudent', error: null });
+    return res.status(200).send({
+      message: `success, ${studentCount} Data Found`,
+      error: null,
+      data: allStudent,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ message: "failure", error: "Internal server error", data: [] });
+  }
 };
-const getStudentById=async(req,res)=>{
-  return res.status(200).send({ message: 'getStudentById', error: null });
-};
-const updateStudentById=async(req,res)=>{
-  return res.status(200).send({ message: 'updateStudentById', error: null });
-};
-const deleteStudentById=async(req,res)=>{
-  return res.status(200).send({ message: 'deleteStudentById', error: null });
+const getStudentById = async (req, res) => {
+  const studentId = req.params.id;
+  if (mongoose.isValidObjectId(studentId)) {
+    try {
+      const singleStudent = await studentServices.getDataById(studentId);
+
+      if (!singleStudent) {
+        return res.status(404).send({
+          message: "No Student found by the id",
+          error: null,
+          data: [],
+        });
+      }
+      return res
+        .status(200)
+        .send({ message: "success", error: null, data: singleStudent });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ message: "failure", error: "Internal server error", data: [] });
+    }
+  }
+  return res
+    .status(400)
+    .send({ message: "failure", error: "id is not valid", data: null });
 };
 
-module.exports = {addStudent,getStudent,getStudentById,updateStudentById,deleteStudentById};
+const updateStudentById = async (req, res) => {
+  const studentId = req.params.id;
+  if (mongoose.isValidObjectId(studentId)) {
+    try {
+      const { name, course, section } = req.body;
+      const oldStudent = await studentServices.getDataById(studentId);
+      if (!oldStudent) {
+        return res.status(400).send({
+          message: "failure",
+          error: "No old Student found by the id",
+          data: null,
+        });
+      }
+      const updatedOldStudent = await studentServices.updateDataById(
+        studentId,
+        {
+          name,
+          course,
+          section,
+        }
+      );
+      return res.status(200).send({  message: "success", error: null,data: updatedOldStudent });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ message: "failure", error: "internal server error", data: null });
+    }
+  }
+  return res.status(400).send({ message: "failure", error: "id is not valid", data: null });
+};
+const deleteStudentById = async (req, res) => {
+  const studentId = req.params.id;
+
+  if (mongoose.isValidObjectId(studentId)) {
+    try {
+      const deletedStudent = await studentServices.deleteDataById(studentId);
+      if (!deletedStudent) {
+        return res.status(400).send({
+          message: "failure",
+          error: "No old Student found by the id",
+          data: null,
+        });
+      }
+      return res.status(200).send({  message: "success", error: null,data: deletedStudent });
+    } catch (error) {
+      return res
+      .status(500)
+      .send({ message: "failure", error: "internal server error", data: null });
+    }
+  }
+  return res.status(400).send({ message: "failure", error: "id is not valid", data: null });
+};
+
+module.exports = {
+  addStudent,
+  getStudent,
+  getStudentById,
+  updateStudentById,
+  deleteStudentById,
+};
--- SERVER/routes/student.route.js
+++ SERVER/routes/student.route.js
@@ -3,7 +3,7 @@ const studentController =require('../controllers/student.controller')
 
 
 
-router.post('/',studentController.addStudent); //get All Student
+router.get('/',studentController.getStudent); //get All Student
 router.get('/:id',studentController.getStudentById); //get Single Student by Id
 router.post('/',studentController.addStudent);// Insert Single new Student Data
 router.put('/:id',studentController.updateStudentById);  // Update Single old Student Data
