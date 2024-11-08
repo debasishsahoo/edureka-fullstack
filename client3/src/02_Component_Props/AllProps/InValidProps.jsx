35e2297703bc4830540c5a8070417a55f26adc02
debasish <debasish.sahoo96@gmail.com>
2024-11-08 15:56:29 +0530

    New Update

---
+++ client3/src/02_Component_Props/AllProps/InValidProps.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import propTypes from 'prop-types'
+import "./InValid.css";
+const Percentage = ({ label, score = 0, total = Math.max(1, score) }) => {
+  return (
+    <div className="pricetab">
+      <h1>{label}</h1>
+      <div className="price">
+        <h2>{Math.round((score / total) * 100)}%</h2>
+      </div>
+    </div>
+  );
+};
+
+//Check If the Value is Numeric or Finite number or numeric String
+function isNumeric(value) {
+  const regex = /^(\+|-)?((\d*\.?\d+)|(\d+\.?\d*))$/;
+  return (
+    Number.isFinite(value) || (typeof value === "string" && regex.test(value))
+  );
+}
+
+//Checks if the Value is Non Zero and Convert value to a number
+function isNonZero(value) {
+  return +value !== 0;
+}
+
+function ValidationType(...validators) {
+  return function (props, propsName, componentName) {
+    const value = props[propsName];
+
+    const Valid = validators.every((validator) => {
+      if (typeof validator === "function") {
+        const result = validator(value);
+        return typeof result === "boolean" && result;
+      }
+      return false;
+    });
+
+    if (!Valid) {
+      return new Error(
+        `Invalid props\`${propsName}\`
+        passed to \`${componentName}\`
+        Validation Failed.Please Fix this First`
+      );
+    }
+  };
+}
+
+
+Percentage.propTypes={
+  label:propTypes.string.isRequired,
+  score:ValidationType(isNumeric),
+  total:ValidationType(isNumeric,isNonZero),
+
+}
+
+
+
+export default Percentage;
--- client3/src/02_Component_Props/ComponentPropsMain.jsx
+++ client3/src/02_Component_Props/ComponentPropsMain.jsx
@@ -1,51 +1,57 @@
 import React from "react";
-import SingleProps from "./AllProps/SingleProps";
-import MultipleProps from "./AllProps/MultipleProps";
-import ValueProps from "./AllProps/ValueProps";
-import PropsPass from "./AllProps/PropsPass";
-import OptionalProps from "./AllProps/OptionalProps";
-import PropsSpread from "./AllProps/PropsSpread";
-import ParentComponent from "./AllProps/ParentComponent";
+// import SingleProps from "./AllProps/SingleProps";
+// import MultipleProps from "./AllProps/MultipleProps";
+// import ValueProps from "./AllProps/ValueProps";
+// import PropsPass from "./AllProps/PropsPass";
+// import OptionalProps from "./AllProps/OptionalProps";
+// import PropsSpread from "./AllProps/PropsSpread";
+// import ParentComponent from "./AllProps/ParentComponent";
+import Percentage from './AllProps/InValidProps'
 const ComponentPropsMain = () => {
-  const name = "Debasish";
-  const age = 36;
+  // const name = "Debasish";
+  // const age = 36;
 
-  const arr = { a: "React", b: "Angular" };
+  // const arr = { a: "React", b: "Angular" };
 
   return (
+    // <div>
+    //   <SingleProps name="Debasish" />
+    //   <br />
+    //   <MultipleProps name="Debasish" title="Sahoo" />
+    //   <br />
+    //   <ValueProps
+    //     str="This is String Value"
+    //     num={23}
+    //     bool={true}
+    //     obj={{ name: "Debasish", title: "Sahoo" }}
+    //     arr={["a", "b", "c"]}
+    //     temp={`Hello i am ${name}, my age is ${age}`}
+    //     jsx={5 + 6 + 7 - (2 * 5) / 7}
+    //   />
+    //   <br />
+    //   <PropsPass students={["Saheli", "Srabani", "Anit", "Debasish"]} />
+    //   <br />
+    //   <OptionalProps student="Anit" />
+    //   <br />
+    //   <OptionalProps />
+    //   <br />
+    //   <PropsSpread a={arr.a} b={arr.b} />
+    //   <br />
+    //   <PropsSpread {...arr} />
+    //   <br />
+    //   <ParentComponent>
+    //     <span>
+    //       Hi i'm Child
+    //     </span>
+    //     <div>
+    //       <h1> This Child Component</h1>
+    //     </div>
+    //   </ParentComponent>
+    // </div>
     <div>
-      <SingleProps name="Debasish" />
-      <br />
-      <MultipleProps name="Debasish" title="Sahoo" />
-      <br />
-      <ValueProps
-        str="This is String Value"
-        num={23}
-        bool={true}
-        obj={{ name: "Debasish", title: "Sahoo" }}
-        arr={["a", "b", "c"]}
-        temp={`Hello i am ${name}, my age is ${age}`}
-        jsx={5 + 6 + 7 - (2 * 5) / 7}
-      />
-      <br />
-      <PropsPass students={["Saheli", "Srabani", "Anit", "Debasish"]} />
-      <br />
-      <OptionalProps student="Anit" />
-      <br />
-      <OptionalProps />
-      <br />
-      <PropsSpread a={arr.a} b={arr.b} />
-      <br />
-      <PropsSpread {...arr} />
-      <br />
-      <ParentComponent>
-        <span>
-          Hi i'm Child
-        </span>
-        <div>
-          <h1> This Child Component</h1>
-        </div>
-      </ParentComponent>
+      {/* <Percentage label="Calculation 1" total={0} score={230}/> */}
+      {/* <Percentage  total={100} score={96}/> */}
+      <Percentage label="Calculation 2" score='a'/>
     </div>
   );
 };
