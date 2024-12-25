d92175b7cd42d990e25001fbfaebba5dbd83ec86
debasish <debasish.sahoo96@gmail.com>
2024-12-25 19:19:38 +0530

    new Update

--- project/client/src/Components/User/ChangePassword.jsx
+++ project/client/src/Components/User/ChangePassword.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
 
 const ChangePassword = () => {
-  return (
-    <div>ChangePassword</div>
-  )
-}
+  const { changePassword,signOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    oldPassword: "",
+    newPassword: "",
+  });
+  const [error, setError] = useState(null);
+  const handelChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await changePassword(credentials);
+      setCredentials({ ...credentials, [e.target.name]: '' });
+      alert('Password Changed Successfully')
+      signOut();
+      navigate("/");
+    } catch (error) {
+      setError(error.response?.data?.message || "Sign in Failed");
+    }
+  };
 
-export default ChangePassword
+  return (
+      <div className="register">
+        <h2>Change Password</h2>
+        <form onSubmit={handelSubmit}>
+          <input
+            type="password"
+            name="oldPassword"
+            placeholder="Old Password"
+            value={credentials.oldPassword}
+            onChange={handelChange}
+            required
+          />
+          <input
+            type="password"
+            name="newPassword"
+            placeholder="New Password"
+            value={credentials.newPassword}
+            onChange={handelChange}
+            required
+          />
+          <button type="submit">Change Password</button>
+          <p>
+            Please <Link to="/">login Here</Link>
+          </p>
+        </form>
+        {error && <p className="error">{error}</p>}
+      </div>
+    );
+};
+
+export default ChangePassword;
--- project/client/src/Components/User/SignOut.jsx
+++ project/client/src/Components/User/SignOut.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import { AuthContext } from "../../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const SignOut = () => {
+const { signOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handelSubmit = async () => {
+    signOut()
+    localStorage.removeItem('token');
+    navigate('/')
+  };
+
   return (
-    <div>SignOut</div>
+    <button onClick={handelSubmit}>SignOut</button>
   )
 }
 
